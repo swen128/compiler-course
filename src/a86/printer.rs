199d72101f24011a7e192f5b7285fe164d3758f8
PND <fujjisaaan@gmail.com>
2023-12-02 04:19:22 +0000

    Fix the a86 printer.

--- src/a86/printer.rs
+++ src/a86/printer.rs
@@ -9,9 +9,12 @@ pub enum Platform {
     MacOS,
 }
 
-
 pub fn print(program: &Program, context: &CompilationContext) -> String {
     let mut output = String::new();
+
+    output.push_str("\tdefault rel\n" );
+    output.push_str("\tsection .text\n" );
+
     for statement in &program.statements {
         output.push_str(&print_statement(&statement, &context));
         output.push_str("\n");
@@ -21,40 +24,44 @@ pub fn print(program: &Program, context: &CompilationContext) -> String {
 
 fn print_statement(statement: &Statement, context: &CompilationContext) -> String {
     match statement {
-        Statement::Global { name } => format!("global {}", name),
-        Statement::Label { name } => format!("{}:", name),
+        Statement::Global { name } => format!("\tglobal {}", name),
+        Statement::Label { name } => format!("{}:", print_label(name, context)),
         Statement::Mov { dest, src } => print_mov(dest, src),
         Statement::Cmp { dest, src } => print_cmp(dest, src),
-        Statement::Je { label } => format!("je {}", label),
-        Statement::Push { src } => format!("push {}", print_operand(src)),
-        Statement::Pop { dest } => format!("pop {}", print_operand(dest)),
+        Statement::Je { label } => format!("\tje {}", print_label(label, context)),
+        Statement::Push { src } => format!("\tpush {}", print_operand(src)),
+        Statement::Pop { dest } => format!("\tpop {}", print_operand(dest)),
         Statement::Add { dest, src } => print_add(dest, src),
         Statement::Sub { dest, src } => print_sub(dest, src),
-        Statement::Call { label } => print_label(label, &context),
-        Statement::Ret => "ret".to_string(),
+        Statement::Call { label } => print_call(label, context),
+        Statement::Ret => "\tret".to_string(),
     }
 }
 
 fn print_mov(dest: &Operand, src: &Operand) -> String {
-    format!("mov {}, {}", print_operand(dest), print_operand(src))
+    format!("\tmov {}, {}", print_operand(dest), print_operand(src))
 }
 
 fn print_cmp(dest: &Operand, src: &Operand) -> String {
-    format!("cmp {}, {}", print_operand(dest), print_operand(src))
+    format!("\tcmp {}, {}", print_operand(dest), print_operand(src))
 }
 
 fn print_add(dest: &Operand, src: &Operand) -> String {
-    format!("add {}, {}", print_operand(dest), print_operand(src))
+    format!("\tadd {}, {}", print_operand(dest), print_operand(src))
 }
 
 fn print_sub(dest: &Operand, src: &Operand) -> String {
-    format!("sub {}, {}", print_operand(dest), print_operand(src))
+    format!("\tsub {}, {}", print_operand(dest), print_operand(src))
+}
+
+fn print_call(label: &String, context: &CompilationContext) -> String {
+    format!("\tcall {}", print_label(label, &context))
 }
 
 fn print_label(label: &String, context: &CompilationContext) -> String {
     match context.platform {
-        Platform::Linux => format!("_{}", label),
-        Platform::MacOS => label.to_string(),
+        Platform::MacOS => format!("_{}", label),
+        Platform::Linux => label.to_string(),
     }
 }
 
