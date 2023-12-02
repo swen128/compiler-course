8a9ab8affa39a423d299da5b5deafef05d0c75fb
PND <fujjisaaan@gmail.com>
2023-12-02 04:20:14 +0000

    Representing the tri function in asm AST.

--- out/asm.s
+++ out/asm.s
@@ -1,4 +1,17 @@
-global entry
+	default rel
+	section .text
+	global entry
 entry:
-mov rax, 12
-ret
+	mov rbx, 36
+tri:
+	cmp rbx, 0
+	je end
+	push rbx
+	sub rbx, 1
+	call tri
+	pop rbx
+	add rax, rbx
+	ret
+end:
+	mov rax, 0
+	ret
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,67 @@
 pub mod a86;
 
-use a86::ast::{
-    Statement::*,
-    Operand::*,
-    Register::*,
-};
+use a86::ast::{Operand::*, Program, Register::*, Statement::*};
 
-use a86::printer::{
-    CompilationContext,
-    Platform,
-};
+use a86::printer::{CompilationContext, Platform};
 
 fn main() {
-    let program = a86::ast::Program {
-        statements: vec![
-            Global { name: "entry".to_string() },
-            Label { name: "entry".to_string() },
-            Mov {
-                dest: Register(RAX),
-                src: Immediate(12),
-            },
-            Ret,
-        ],
-    };
-    
+    let program = tri(36);
+
     let context = CompilationContext {
         platform: Platform::Linux,
     };
-    
+
     println!("{}", a86::printer::print(&program, &context));
     println!("{}", a86::interpreter::interpret(&program));
+}
+
+fn tri(n: i64) -> Program {
+    let statements = vec![
+        Global {
+            name: "entry".to_string(),
+        },
+        Label {
+            name: "entry".to_string(),
+        },
+        Mov {
+            dest: Register(RBX),
+            src: Immediate(n),
+        },
+        Label {
+            name: "tri".to_string(),
+        },
+        Cmp {
+            dest: Register(RBX),
+            src: Immediate(0),
+        },
+        Je {
+            label: "end".to_string(),
+        },
+        Push { src: Register(RBX) },
+        Sub {
+            dest: Register(RBX),
+            src: Immediate(1),
+        },
+        Call {
+            label: "tri".to_string(),
+        },
+        Pop {
+            dest: Register(RBX),
+        },
+        Add {
+            dest: Register(RAX),
+            src: Register(RBX),
+        },
+        Ret,
+        Label {
+            name: "end".to_string(),
+        },
+        Mov {
+            dest: Register(RAX),
+            src: Immediate(0),
+        },
+        Ret,
+    ];
+
+    Program { statements }
 }
